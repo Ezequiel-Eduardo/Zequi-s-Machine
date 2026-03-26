f4b1b5a686fff709a9ed7d235493af08a5ce3684
Ezequiel-Eduardo <ezequiel.santos@sptech.school>
2026-03-25 22:21:34 -0300

    Adição da funcionalidade de digitar números
    Agora o usuário consegue inserir números na input através da parte visual da calculadora. Também validei se o número digitado é válido para a base escolhida

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,52 @@
-const results = document.querySelectorAll(".result");
+let primeiraDigitada = true;
 
-results.forEach((item) => {
-  item.addEventListener("click", () => {
-    
-    results.forEach((el) => el.classList.remove("active"));
-    
-    item.classList.add("active");
+let results = document.querySelectorAll(".result");
+
+results.forEach((result) => {
+  result.addEventListener("click", function () {
+    results.forEach((r) => r.classList.remove("active"));
+    this.classList.add("active");
   });
 });
+
+function digitar(valorDigitado) {
+  let ativo = document.querySelector(".result.active");
+  let base = ativo.querySelector("span").textContent;
+
+  let permitido = false;
+
+  if (base == "BIN") {
+    if (valorDigitado == 0 || valorDigitado == 1) {
+      permitido = true;
+    }
+  } else if (base == "HEX") {
+    if (
+      valorDigitado <= 9 ||
+      valorDigitado == "A" ||
+      valorDigitado == "B" ||
+      valorDigitado == "C" ||
+      valorDigitado == "D" ||
+      valorDigitado == "E" ||
+      valorDigitado == "F"
+    ) {
+      permitido = true;
+    }
+  } else if (base == "DEC") {
+    if (valorDigitado <= 9) {
+      permitido = true;
+    }
+  } else if (base == "OCT") {
+    if (valorDigitado <= 7) {
+      permitido = true;
+    }
+  }
+
+  if (permitido) {
+    if (primeiraDigitada) {
+      input_number.innerHTML = "";
+      primeiraDigitada = false;
+    }
+
+    input_number.innerHTML += valorDigitado;
+  }
+}
